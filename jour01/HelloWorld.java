901d9383bd5a11eeabf29e5c5954f838e1c61360
xavier-delfini <xavier.delfini@laplateforme.io>
2023-05-09 11:34:28 +0200

    Finalisation du job3

--- jour01/HelloWorld.java
+++ jour01/HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args){
+        //Fonction pour afficher Hello World dans la console
     System.out.println("Hello World");
     }
+    /*Ceci
+    est
+    un
+    commentaire
+     */
 }
