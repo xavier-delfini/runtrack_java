e72d01639e1dfa5e765043b2de31874c6c10c05d
xavier-delfini <xavier.delfini@laplateforme.io>
2023-05-09 11:40:15 +0200

    Finalisation du job04

---
+++ jour01/DeclarationVariable.java
@@ -0,0 +1,9 @@
+public class DeclarationVariable {
+    public static void main(String[] args) {
+        int a,b,c;
+        a=b=c= 0;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
