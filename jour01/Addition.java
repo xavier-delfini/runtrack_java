b820698ccfd07234a952e287e7d11bea5e4a9557
xavier-delfini <xavier.delfini@laplateforme.io>
2023-05-09 12:02:54 +0200

    Finalisation du Job07

---
+++ jour01/Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+    public static void main(String[] args) {
+        float num1 =3;
+        float num2= 73;
+        float num3 = num1 /num2;
+        System.out.println(num3);
+    }
+}
