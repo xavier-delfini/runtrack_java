5ce8235048d23498ec769a80a4257b2e45a704a7
xavier-delfini <xavier.delfini@laplateforme.io>
2023-05-09 12:21:48 +0200

    Finalisation du Job09

---
+++ jour01/MyString.java
@@ -0,0 +1,10 @@
+public class MyString {
+    public static void main(String[] args) {
+        String String1= "Hello";
+        final String String2 = "Hellow";
+        String str3 = String.valueOf("Helloo");
+        String String4 = new String("Helo");
+
+
+    }
+}
