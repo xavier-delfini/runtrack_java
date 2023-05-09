57edd12f73350a0efb7d0e0692066089f0c04258
xavier-delfini <xavier.delfini@laplateforme.io>
2023-05-09 11:44:37 +0200

    Finalisation du job 05

---
+++ jour01/Calcul.java
@@ -0,0 +1,10 @@
+public class Calcul {
+    public static void main(String[] args){
+        int nombre = 7;
+        System.out.println(nombre);
+        nombre+=3;
+        System.out.println(nombre);
+        nombre*=5;
+        System.out.println(nombre);
+    }
+}
