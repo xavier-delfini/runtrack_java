ae0997d455f73fea5015c2e2521147733196559d
xavier-delfini <xavier.delfini@laplateforme.io>
2023-05-10 10:11:29 +0200

    Finalisation du job01

---
+++ jour02/Comptage.java
@@ -0,0 +1,9 @@
+public class Comptage {
+    public static void main(String[] args){
+        int i = 0;
+        while (i<100){
+            i++;
+            System.out.println(i);
+        }
+    }
+}
