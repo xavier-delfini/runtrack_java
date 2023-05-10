1cb7934cc80f7b6a8d221cf9617a07ffafb4a19c
xavier-delfini <xavier.delfini@laplateforme.io>
2023-05-10 10:13:53 +0200

    Finalisation du job02

---
+++ jour02/ComptageInverser.java
@@ -0,0 +1,10 @@
+public class ComptageInverser {
+    public static void main(String[] args) {
+        int i = 100;
+        while (i >= 0) {
+            System.out.println(i);
+            i--;
+
+        }
+    }
+}
