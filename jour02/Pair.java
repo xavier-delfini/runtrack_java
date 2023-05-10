47c14be4778b86215dcf649d6e2e36d136dc6e8c
xavier-delfini <xavier.delfini@laplateforme.io>
2023-05-10 10:32:15 +0200

    Finalisation du Job04

---
+++ jour02/Pair.java
@@ -0,0 +1,11 @@
+public class Pair {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i != 101) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
