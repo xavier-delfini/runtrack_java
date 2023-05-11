638b6df2a3557568df02adbf36c02fffaea0c286
xavier-delfini <xavier.delfini@laplateforme.io>
2023-05-11 11:22:17 +0200

    Finalisation du job01

---
+++ jour03/Tableau.java
@@ -0,0 +1,16 @@
+
+import java.util.Arrays;
+public class Tableau {
+    public static void main(String[] args){
+        //Job1
+        int[] tableau = new int[5];
+        tableau[0] = 10;
+        tableau[2] = 2;
+        tableau[4] =69;
+        System.out.println(Arrays.toString(tableau));
+        System.out.println(tableau[1]);
+        //Job2
+
+
+    }
+}
